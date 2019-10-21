85267e69d9287d57c43e9c249bbd42844946c4d7
Jiben <jibenapps@gmail.com>
2019-10-21 12:07:07 +0200

    1st commit

---
+++ main.ino
@@ -0,0 +1,59 @@
+#include <SoftwareSerial.h>
+
+#define RxD 10 //Pin 10 pour arduino RX --> vers TxD de la carte Bluetooth
+#define TxD 11 //Pin 11 pour arduino TX --> vers RxD de la carte Bluetooth 
+
+SoftwareSerial BTSerial(10, 11); // RX | TX
+
+void setup() {
+  InitCommunicationSerie();
+  Serial.println("Initialisation carte : Ok");
+
+  // Configuration du bluetooth
+  pinMode(RxD, INPUT);
+  pinMode(TxD, OUTPUT);
+
+  InitCommunicationBluetoothSerie();
+}
+
+// Initialiastion de la communication serie avec l'ordinateur
+
+void InitCommunicationSerie() {
+  Serial.begin(9600);
+  while(!Serial) {
+    
+  }
+  Serial.println("Demarrage connexion serie : Ok");
+}
+
+void InitCommunicationBluetoothSerie() {
+  BTSerie.begin(9600); //38400 / 57600 / 38400
+  while(!BTSerie) {
+    Serial.println("Attente reponse Bluetooth");
+  }
+  Serial.println("Demarrage connexion Bluetooth serie : Ok");
+}
+
+void loop() {
+  char c;
+
+  if(BTSerie.available()) {
+    Serial.println("Reception de : ");
+    while(BTSerie.available()) {
+      c = BTSerie.read();
+      Serial.println(c);
+    }
+  }
+
+  if(Serial.available()) {
+    Serial.println("Envoie vers BLuetooth : ");
+    while(Serial.available()) {
+      c = Serial.read();
+      Serial.print(c);
+      BTSerie.print(c);
+    }
+  }
+
+  delay(1000);
+
+}
